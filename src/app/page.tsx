dbffb0d4413f6dde3f94ca8cf4538ae06de3859d
mainul1 <ac.mainul.mmb@gmail.com>
2024-12-17 22:49:17 -0500

    Update configuration and dependencies to resolve build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import Hero from '../components/hero'
-import About from '../components/about'
-import Research from '../components/research'
-import Publications from '../components/publications'
-import Contact from '../components/contact'
-import Footer from '../components/footer'
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <Hero />
-      <About />
-      <Research />
-      <Publications />
-      <Contact />
-      <Footer />
+    <div>
+      <h1>Dr. Md Mainul Hasan Sarker</h1>
+      <p>Welcome to my personal website</p>
     </div>
   )
 }
